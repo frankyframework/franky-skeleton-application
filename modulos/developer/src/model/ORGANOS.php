6f5b7b375db632a5da33f29242608f43380261cf
ussiel <ussiel@gmail.com>
2019-09-26 23:55:33 -0500

    ajustes

--- modulos/developer/src/model/ORGANOS.php
+++ modulos/developer/src/model/ORGANOS.php
@@ -3,93 +3,107 @@ namespace Developer\model;
 
 class ORGANOS  extends \Franky\Database\Mysql\objectOperations
 {
+    private $busca;
 
-  public function __construct()
-  {
-    parent::__construct();
-    $this->from()->addTable('franky');
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->from()->addTable('franky');
+        $this->busca = '';
+    }
 
+    function setBusca($busca)
+    {
+        $this->busca = $busca;
+    }
 
-        function getData($id='',$nombre='',$url='',$status='1',$delete='1')
+    function getData($id='',$nombre='',$url='',$status='1',$delete='1')
+    {
+        $campos = array("nombre","url","id","css","js","jquery","php","permisos","constante","ajax","modulo","status");
+
+        if(!empty($id))
         {
-            $campos = array("nombre","url","id","css","js","jquery","php","permisos","constante","ajax","modulo","status");
-
-            if(!empty($id))
+            if(is_numeric($id))
             {
-                if(is_numeric($id))
-                {
-                  $this->where()->addAnd('id',$id,'=');
-                }
-                else
-                {
-                  $this->where()->addAnd('constante',$id,'=');
-                }
+              $this->where()->addAnd('id',$id,'=');
             }
-            if(!empty($url))
+            else
             {
-              $this->where()->addAnd('urlo',"%$url%",'like');
+              $this->where()->addAnd('constante',$id,'=');
             }
-            if(!empty($nombre))
-            {
-              $this->where()->addAnd('nombre',"%$nombre%",'like');
-            }
-            if($delete != "")
-            {
-              $this->where()->addAnd('editable',$delete,'=');
-            }
-             if($status != "")
-            {
-              $this->where()->addAnd('status',$status,'=');
-            }
-
-            return $this->getColeccion($campos);
-
+        }
+        if(!empty($url))
+        {
+          $this->where()->addAnd('url',"%$url%",'like');
+        }
+        if(!empty($nombre))
+        {
+          $this->where()->addAnd('nombre',"%$nombre%",'like');
+        }
+        if($delete != "")
+        {
+          $this->where()->addAnd('editable',$delete,'=');
+        }
+         if($status != "")
+        {
+          $this->where()->addAnd('status',$status,'=');
         }
 
-        function findPagina($campo, $valor,$id)
+        if(!empty($this->busca))
         {
-            $campos = array("id");
-            $this->where()->addAnd('status','1','=');
-            $this->where()->addAnd($campo,$valor,'=');
-            if(!empty($id))
-            {
-              $this->where()->addAnd('id',$id,'<>');
-            }
-
-
-            return $this->getColeccion($campos);
-
-
+              $this->where()->concat('AND (');
+              $this->where()->addOr('nombre','%'.$this->busca.'%','like');
+              $this->where()->addOr('url','%'.$this->busca.'%','like');
+              $this->where()->concat(')');
         }
 
-        private function optimizeEntity($array)
+        return $this->getColeccion($campos);
+
+    }
+
+    function findPagina($campo, $valor,$id)
+    {
+        $campos = array("id");
+        $this->where()->addAnd('status','1','=');
+        $this->where()->addAnd($campo,$valor,'=');
+        if(!empty($id))
         {
-            foreach ($array as $k => $v )
-            {
-                if (!isset($v)) {
-                    unset($array[$k]);
-                }
-            }
-            return $array;
+          $this->where()->addAnd('id',$id,'<>');
         }
 
-        public function save($organos)
+
+        return $this->getColeccion($campos);
+
+
+    }
+
+    private function optimizeEntity($array)
+    {
+        foreach ($array as $k => $v )
         {
-
-            $organos = $this->optimizeEntity($organos);
-
-            if (isset($organos['id']))
-            {
-                $this->where()->addAnd('id',$organos['id'],'=');
-                return $this->editarRegistro($organos);
+            if (!isset($v)) {
+                unset($array[$k]);
             }
-            else {
-
-                return $this->guardarRegistro( $organos);
-            }
-
         }
+        return $array;
+    }
+
+    public function save($organos)
+    {
+
+        $organos = $this->optimizeEntity($organos);
+
+        if (isset($organos['id']))
+        {
+            $this->where()->addAnd('id',$organos['id'],'=');
+            return $this->editarRegistro($organos);
+        }
+        else {
+
+            return $this->guardarRegistro( $organos);
+        }
+
+    }
 }
 
 ?>
