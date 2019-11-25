9aaa03dd6a5deb0fb0a1fdd6af71ed57be62941b
ussiel <ussiel@gmail.com>
2019-11-24 23:51:30 -0600

    Admin custom attributes

--- modulos/catalog/menu/admin.php
+++ modulos/catalog/menu/admin.php
@@ -1,6 +1,11 @@
 <?php
 return array(
-     'title'=> "Catalogo",
+    'title'=> "Catalogo",
+    array(
+     "permiso" =>   ADMINISTRAR_PRODCUT_CATALOG,
+     "url" => $MyRequest->url(ADMIN_CATALOG_PRODUCT),
+     "etiqueta" => "Productos"
+    ),
     array(
      "permiso" =>   ADMINISTRAR_CATEGORY_CATALOG,
      "url" => $MyRequest->url(ADMIN_CATALOG_CATEGORY),
