5fefe36dbffda9bdf0a58a35762c594cf5d066c8
Alberto <oxtlimail@gmail.com>
2019-12-03 20:58:26 -0600

    Ajuste estilos widget catalog

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,13 +10,13 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: { // destination file and source file
-          "public/skin/default/css/style.css": "public/skin/default/less/style.less", "public/skin/default/css/grid.css": "public/skin/default/less/grid.less", "public/skin/default/css/panel.css": "public/skin/default/less/panel.less", "public/skin/ecommerce/css/cart.css": "public/skin/ecommerce/less/cart.less",
+          "public/skin/default/css/style.css": "public/skin/default/less/style.less", "public/skin/default/css/grid.css": "public/skin/default/less/grid.less", "public/skin/default/css/panel.css": "public/skin/default/less/panel.less", "public/skin/ecommerce/css/cart.css": "public/skin/ecommerce/less/cart.less","public/skin/catalog/css/catalog.css": "public/skin/catalog/less/catalog.less",
         }
       }
     },
     watch: {
       less: {
-            files: ['public/skin/default/less/*.less', 'public/skin/ecommerce/less/*.less'],
+            files: ['public/skin/default/less/*.less', 'public/skin/ecommerce/less/*.less', 'public/skin/catalog/less/*.less'],
             tasks: ['less'],
             options: {
                 livereload: true
