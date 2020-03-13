4e24670232f112de8546db02652c0877e19e1297
Alberto <oxtlimail@gmail.com>
2020-03-12 19:48:05 -0600

    Se menú categorias - catalogo - dinámico

--- modulos/franky/configure/blocks.php
+++ modulos/franky/configure/blocks.php
@@ -1,8 +1,13 @@
 <?php
 return json_encode([
-   /* [
+/*    [
         'position' => 'after',
         'reference' => '.menu_web',
         'html' => getCatalogBuscadorPrincipal()
-    ]*/
+    ], */
+    [
+        'position' => 'after',
+        'reference' => '._menu',
+        'html' => getCategoryMenu()
+    ],
 ]);
