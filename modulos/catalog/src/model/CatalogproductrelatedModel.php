2ebb9966c8130d3c6bb64131e34a7994ff333ebb
ussiel <ussiel@gmail.com>
2021-01-09 00:02:35 -0600

    control

--- modulos/catalog/src/model/CatalogproductrelatedModel.php
+++ modulos/catalog/src/model/CatalogproductrelatedModel.php
@@ -22,7 +22,7 @@ class CatalogproductrelatedModel  extends \Franky\Database\Mysql\objectOperation
     {
         $data = $this->optimizeEntity($data);
         $campos = ["id_parent","id_product","name","sku",'images','url_key',
-            "price","stock","iva","incluye_iva","saleable","in_stock","min_qty"];
+            "price","stock","iva","incluye_iva","saleable","in_stock","min_qty","stock_infinito"];
 
         foreach($data as $k => $v)
         {
